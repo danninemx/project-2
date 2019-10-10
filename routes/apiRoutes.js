feaa673821884044cda78407de0cc36ded8c8283
Bladimir Orellana <me@bladimirorellana.com>
2019-10-09 23:06:48 -0500

    Create api for cssData to get all rows and api for single row and and i alse create the cssData file in the models folder

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -14,7 +14,19 @@ const Op = Sequelize.Op; // Sequelize querying operator
 
 
 module.exports = function (app) {
+//API for all data in table cssData
+app.get('/api/css/properties',(req,res)=>{
+  db.cssData.findAll({ attributes: ['id', 'properties','definitions']}).then((result)=>{
+    res.json(result)
+  })
+}) 
 
+//API for single result in table cssData
+app.get('/api/css/properties/:name',(req,res)=>{
+  db.cssData.findAll({where: {properties: req.params.name}}).then((result)=>{
+    res.json(result)
+  })
+}) 
 
   // Get contents for lesson email
   app.get("/api/email/:lessonId", function (req, res) {
