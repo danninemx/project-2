27eb8c48fa5affe4c83efe2c0088f837be84ad3b
Danny Kim <dudkny@gmail.com>
2019-10-10 03:17:47 -0500

    Updated mailer with templates and logic for users that finished all lessons

--- dayly-email/email-sender.js
+++ dayly-email/email-sender.js
@@ -3,13 +3,53 @@ var db = require("../models");
 const cron = require('node-cron');
 'use strict';
 const nodemailer = require('nodemailer');
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op; // Sequelize querying operator
 
 // Deployed site URL for link in email
-let deployURL = 'https://immense-ridge-78589.herokuapp.com/';
+let deployURL = 'https://immense-ridge-78589.herokuapp.com';
 
 //------------//
 // Nodemailer //
 //------------//
+
+// Call this to email user who finished all lessons
+function maxSend(fn, em) {
+
+  // Create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'projectgroupLynx@gmail.com',
+      pass: 'project-2'
+    }
+  });
+
+  // *** TO-DO: Change the temporarily placed API route to HTML route for study page. *** //
+  let contents = `
+<h2>Great job, ${fn}!</h2>
+</br>
+<p>You have finished all of the lessons available at this time.</p>
+</br>
+<p>We will check you know tomorrow if we have anything else.</p>
+</br>
+<p>In the mean time, feel free to explore <a href='${deployURL}/api/js/methods'>these additional resources</a>!</p>
+</br>
+<p>Sincerely,</p>
+<h3>Your friends at <strong>Group Lynx</strong></h3>
+`;
+
+  // Send mail with defined transport object
+  transporter.sendMail({
+    from: '"Group Lynx 👻" <projectgroupLynx@gmail.com>', // sender address
+    to: em,
+    cc: 'projectgrouplynx@gmail.com',
+    subject: "Kudos from Group Lynx!",
+    html: contents // message body
+  });
+
+}     // End of maxSend()
+
 
 
 // Call this to send lesson email to all users
@@ -24,66 +64,82 @@ function massSend() {
     }
   });
 
-  // Query the users table for all users
-  db.users.findAll({}).then(function (result) {
+  // Determine the highest lessonId possible
+  // SELECT MAX(lessonId) FROM guides;
+  db.guide.max('lessonId').then(function (maxId) {
+    console.log(chalk.bgYellow('Highest lessonId in guides is :' + maxId));
 
-    // For each user...
-    for (let i = 0; i < result.length; i++) {
+    // Query the users table for all users
+    db.users.findAll({}).then(function (result) {
 
-      let recipientName = result[i].userFirstName;
-      let recipientEmail = result[i].email;
-      let progress = result[i].lastLessonId;
-      let contents = "";
+      // For each user...
+      for (let i = 0; i < result.length; i++) {
 
-      //---------------------------------------------------------//
-      // TO-DO: Increment 1 to users table's lastLessonId value. //
-      //---------------------------------------------------------//
-      // `UPDATE users
-      // SET lastLessonId = 1 
-      // WHERE id = 1;
-      // `
+        // Save user details for later
+        let recipientId = result[i].id;
+        let recipientName = result[i].userFirstName;
+        let recipientEmail = result[i].email;
+        let progress = result[i].lastLessonId;
+        let contents = "";
 
+        // If user already has highest lesson # available...
+        if (progress === maxId) {
+          maxSend(recipientName, recipientEmail);
 
-      // Query the guides table for HTML components needed for the user's lesson email.
-      db.guide.findAll({
-        where: {
-          lessonId: result[i].lastLessonId
-        }
-      }).then(function (res) {
+        } else {
+          // Query the guides table for HTML components needed for the user's lesson email.
+          db.guide.findAll({
+            where: {
+              lessonId: (progress + 1),
+              paragraph: {
+                [Op.lt]: 4 // < 4 rows of data.
+              }
+            }
+          }).then(function (res) {
 
-        // For each HTML component...
-        for (let j = 0; j < res.length; j++) {
+            // For each HTML component...
+            for (let j = 0; j < res.length; j++) {
 
-          // Concatenate new component and a line break
-          contents += res[j].content + "</br>";
+              // Concatenate new component and a line break
+              contents += res[j].content + "</br>";
 
-          console.log(chalk.yellow(contents));
-          console.log(chalk.red(typeof (contents)));
+              console.log(chalk.bgWhite(contents));
 
-          // If the final component was concatenated...
-          if (j === (res.length - 1)) {
+              // If the final component was concatenated...
+              if (j === (res.length - 1)) {
 
-            // Append link to the guide page
-            contents += '<a href="' + deployURL + '">Continue Reading</a>';
+                // Append link to the guide page
+                contents += '<a href="' + deployURL + '">Continue Reading</a>';
 
-            // Send mail with defined transport object
-            transporter.sendMail({
-              from: '"Group Lynx 👻" <projectgroupLynx@gmail.com>', // sender address
-              to: recipientEmail,
-              cc: 'projectgrouplynx@gmail.com',
-              subject: "Today's lesson for " + recipientName,
-              html: contents // lesson contents
-            });
-          }
+                // Send mail with defined transport object
+                transporter.sendMail({
+                  from: '"Group Lynx 👻" <projectgroupLynx@gmail.com>', // sender address
+                  to: recipientEmail,
+                  cc: 'projectgrouplynx@gmail.com',
+                  subject: "Today's lesson for " + recipientName,
+                  html: contents // lesson contents
+                }); // End email transmission
+              } // End concatenation completion check
 
-        } // End loop through email components
-      }); // End of the guides table query
+            } // End loop through email components
+          }); // End of the guides table query
 
-    } // End of the loop through the users
-  })   // End of the users table query
-}     // End of massSend()
+          // Increment 1 to this user's table's lastLessonId value.
+          // UPDATE users SET lastLessonId = (lastLessonId + 1) WHERE lastLessonId < maxId
+          db.users.increment('lastLessonId', {
+            where: {
+              id: recipientId
+            }
+          });
 
+        } // End of condition check for maxSend()
 
+      } // End of the loop through the users
+    }) // End of the users table query
+
+  }); // End of the MAX(guides.lessonId) query
+
+} // End of massSend()
 
 
 //----------------//
